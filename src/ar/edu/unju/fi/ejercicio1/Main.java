03fb1bf94c16e3f3eac03c41d1367cc84dbb89d8
cjch2 <cjch2@DESKTOP-R90GMLH>
2024-04-04 15:16:50 -0300

    Ejercicio 01 Casi resuelto

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num1 = 5;
+		int num2 = 6;
+		int num3 = 7;
+		int num4 = 8;
+		int num5 = 9;
+		
+		double promedio =(double) (num1 +num2+num3+num4+num5)/5;
+		
+		System.out.println("EL promedio es: "+promedio); //Decorar para darle formato bonito
 	}
 
 }
